e86db67fbfee5466c8f2e7e45fd8ba7b161925e3
polrieramcardle <pol.riera.mcardle@estudiantat.upc.edu>
2025-10-19 10:29:59 +0200

    Add files via upload

---
+++ plantejament inicial.py
@@ -0,0 +1,15 @@
+class Camio:
+    '''
+    Conté les coords, els tancs disponibles, els km acumulats i els viatges acumulats
+    Serà modificat cada cop que arrib a una gasolinera o una warehouse. Ha de tenir una ruta pròpia que sigui solució,
+    i que amb l'heurística, s'haurà de modificar en funció dels seu cost-benefici
+    '''
+    pass
+
+class Estat:
+    '''
+    És el conjunt de camions amb tota la seva informació. Aquesta classe representarà l'estat del problema entre accions 
+    dels operadors. Serà aquella modificada pels operadors i contindrà el benefici, el qual hem de maximitzar per mitjà d'una heurísitca.
+
+    '''
+    pass
